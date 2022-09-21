0dc1b380e9aad08d82c8cf1c6ccc7b9548bbae89
ShokhrookhR <shokh7997@gmail.com>
2022-09-21 22:23:58 +0500

    data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './index.scss';
-
-const categories = ['Все', 'Горы', 'Море', 'Архитектура', 'Города'];
+import data from './assets/data.json';
 const pagination = [1, 2, 3];
 
-function Collection({ name, images }) {
+function Collection({ name, photos }) {
   return (
     <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
+      <img className="collection__big" src={photos[0]} alt="Item" />
       <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
+        {photos.map(
+          (img) => img !== photos[0] && <img className="collection__mini" src={img} alt="Item" />,
+        )}
       </div>
       <h4>{name}</h4>
     </div>
@@ -29,27 +28,21 @@ function App() {
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          {categories.map((cat) => (
+          {data.categories.map((cat) => (
             <li
               key={cat}
               onClick={() => onChangeCategory(cat)}
               className={category === cat ? 'active' : ''}>
-              {cat}
+              {cat.name}
             </li>
           ))}
         </ul>
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {data.collections.map((obj) => (
+          <Collection {...obj} />
+        ))}
       </div>
       <ul className="pagination">
         {pagination.map((p) => (
